ef12bd058c9e01b40847e3ed87d40c0aa7cbd164
Grandis SYF <gra@dr.com>
2025-06-03 18:11:45 +0700

    feat: allow Project Users to create stages in kanban view
    - Patch KanbanController to override canCreate logic
    
    - Add new XML view to extend project task kanban permissions

--- static/src/js/project_kanban_controller.js
+++ static/src/js/project_kanban_controller.js
@@ -1,22 +1,38 @@
 /** @odoo-module **/
 
 import { patch } from "@web/core/utils/patch";
-import { ProjectTaskKanbanController } from "@project/views/project_task_kanban/project_task_kanban_controller";
+import { KanbanController } from "@web/views/kanban/kanban_controller";
+import { useService } from "@web/core/utils/hooks";
 
-patch(ProjectTaskKanbanController.prototype, {
-  async onWillStart() {
-    await super.onWillStart();
-    if (this.props.list.isGroupedByStage) {
-      // Check if user has either Project Manager or Project User group
-      const hasProjectManager = await this.userService.hasGroup(
-        "project.group_project_manager",
-      );
-      const hasProjectUser = await this.userService.hasGroup(
-        "project.group_project_user",
-      );
+// Patch the KanbanController to allow Project Users to create stages
+patch(KanbanController.prototype, {
+  setup() {
+    super.setup();
+    this.user = useService("user");
 
-      // Allow stage creation for both groups
-      this.isProjectManager = hasProjectManager || hasProjectUser;
-    }
+    // Override the canCreate property for project kanban views
+    const originalCanCreate = this.canCreate;
+
+    this.canCreate = async () => {
+      // Check if this is a project task view grouped by stage
+      if (
+        this.model.resModel === "project.task" &&
+        this.model.groupBy &&
+        this.model.groupBy.includes("stage_id")
+      ) {
+        // Check if user has either Project Manager or Project User group
+        const hasProjectManager = await this.user.hasGroup(
+          "project.group_project_manager",
+        );
+        const hasProjectUser = await this.user.hasGroup(
+          "project.group_project_user",
+        );
+
+        return hasProjectManager || hasProjectUser;
+      }
+
+      // For other views, use the original logic
+      return originalCanCreate;
+    };
   },
 });
